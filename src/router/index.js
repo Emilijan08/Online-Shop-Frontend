7bc15a490ea96c81eb94ec6f803eff0f894d8931
Emilijan Russ <e.russ@bluewin.ch>
2024-04-10 16:51:40 +0200

    Add FAQ and ShopCartNew Page from Tailwind UI

--- src/router/index.js
+++ src/router/index.js
@@ -30,7 +30,7 @@ const router = createRouter({
     {
       path: "/cart",
       name: "Cart",
-      component: ShopCartView,
+      component: ShopCartNewView,
       meta: {
         requiresAuth: true,
       },
--- src/views/WishList.vue
+++ src/views/WishList.vue
@@ -7,16 +7,23 @@ import { RouterLink } from 'vue-router'
 <template>
   <div>
     <h1>WishList</h1>
-    <a href="../assets/Herz Umriss.png" id="Heart">
-      <img src="../assets/Herz Umriss.png" alt="Heart" />
+    <a
+      href="https://www.psdgraphics.com/wp-content/uploads/2022/01/perfect-heart-shape.png"
+      id="Heart"
+    >
+      <img
+        id="Heart"
+        src="https://www.psdgraphics.com/wp-content/uploads/2022/01/perfect-heart-shape.png"
+        alt="Heart"
+      />
     </a>
   </div>
 </template>
 
 <style scoped>
 #Heart {
-  width: 10px;
-  height: 10px;
+  width: 150px;
+  height: 115px;
   borderbox: 1px solid black;
 }
 </style>
