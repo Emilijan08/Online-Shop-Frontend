33910ee08ad2fc58d7cc07d1c3b2a453bf8a5f41
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-04-15 19:15:07 +0200

    Fix import errors

--- src/router/index.js
+++ src/router/index.js
@@ -1,49 +1,49 @@
-import { createRouter, createWebHistory } from "vue-router";
-import LoginView from "../views/LoginView.vue";
-import RegisterView from "../views/RegisterView.vue";
-import HomeView from "../views/HomeView.vue";
-import ShopCartView from "../views/ShopCartView.vue";
-import ProductView from "../views/ProductView.vue";
-import WishList from "../views/WishList.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import LoginView from '../views/LoginView.vue';
+import ProductView from '../views/ProductView.vue';
+import RegisterView from '../views/RegisterView.vue';
+import ShopCartNewView from '../views/ShopCartNewView.vue';
+import WishListView from '../views/WishListView.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: "/login",
-      name: "Login",
+      path: '/login',
+      name: 'Login',
       component: LoginView,
     },
     {
-      path: "/register",
-      name: "Register",
+      path: '/register',
+      name: 'Register',
       component: RegisterView,
     },
     {
-      path: "/",
-      name: "Home",
+      path: '/',
+      name: 'Home',
       component: HomeView,
       meta: {
         requiresAuth: true,
       },
     },
     {
-      path: "/cart",
-      name: "Cart",
+      path: '/cart',
+      name: 'Cart',
       component: ShopCartNewView,
       meta: {
         requiresAuth: true,
       },
     },
     {
-      path: "/products/:productId",
-      name: "Product",
+      path: '/products/:productId',
+      name: 'Product',
       component: ProductView,
     },
     {
-      path: "/whishlist",
-      name: "Whishlist",
-      component: WishList,
+      path: '/whishlist',
+      name: 'Whishlist',
+      component: WishListView,
       meta: {
         requiresAuth: true,
       },
@@ -51,13 +51,13 @@ const router = createRouter({
   ],
 });
 router.beforeEach((to, from, next) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
 
   if (to.matched.some((record) => record.meta.requiresAuth)) {
     // this route requires auth, check if logged in
     if (!token) {
       // no token, redirect to login page
-      next({ name: "Login" });
+      next({ name: 'Login' });
     } else {
       // token exists, allow access to the route
       next();
