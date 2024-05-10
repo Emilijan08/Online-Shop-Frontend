47bf3da0db972984ddc084260fbe894ea9e960b5
Emilijan Russ <e.russ@bluewin.ch>
2024-05-10 13:43:08 +0200

    Add Functions to Add the Bestbuy API
    (i still have to replace the SIGN UP FOR KEY with my own RapidApi key)

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@ import { RouterView } from 'vue-router'
 import { useProductStore } from './stores/ProductsStore'
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
-import { computed } from 'vue'
+import { computed, ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
+import axios from 'axios'
 
 const route = useRoute()
 const store = useProductStore()
@@ -13,6 +14,37 @@ store.getProducts()
 const showFooter = computed(() => {
   return !['/login', '/register', '/:catchall(.*)*'].includes(route.path)
 })
+
+// Importiere das axios-Paket, um HTTP-Anfragen zu senden
+const axios = require('axios')
+
+// Definiere die Optionen für die HTTP-Anfrage
+const options = {
+  method: 'POST',
+  url: 'https://bestbuyraygorodskijv1.p.rapidapi.com/getAllCategories',
+  headers: {
+    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY', // Ersetze 'SIGN-UP-FOR-KEY' durch deinen eigenen RapidAPI-Schlüssel
+    'X-RapidAPI-Host': 'BestBuyraygorodskijV1.p.rapidapi.com'
+  }
+}
+
+// Versuche, die Anfrage an die API zu senden
+try {
+  const response = await axios.request(options) // Führe die Anfrage aus
+  console.log(response.data) // Gib die Antwortdaten in der Konsole aus
+} catch (error) {
+  console.error(error) // Bei einem Fehler, gib den Fehler in der Konsole aus
+}
+
+export default {
+  setup() {
+    const categories = ref([])
+    onMounted(async () => {
+      categories.value = await getAllCategories()
+    })
+    return { categories }
+  }
+}
 </script>
 
 <template>
