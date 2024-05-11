6a83861922b36eea1634078b77047be626d517d8
Emilijan Russ <e.russ@bluewin.ch>
2024-05-11 21:55:11 +0200

    Change Design of RegisterView/add Background Image

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -46,115 +46,137 @@ const arePasswordsEqual = () => {
 </script>
 
 <template>
-  <div
-    class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 mb-64"
-  >
-    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img
-        class="mx-auto h-10 w-auto"
-        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-        alt="Your Company"
-      />
-      <h2
-        class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+  <body>
+    <div
+      class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 mb-64"
+    >
+      <div
+        class="ElementsWithBackground w-4/12 h-max bg-white justify-center shadow-xl rounded-xl"
       >
-        Register your account
-      </h2>
-    </div>
-
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form
-        class="space-y-6"
-        action="#"
-        method="POST"
-        @submit.prevent="
-          store.register(username, password, confirmPassword, arePasswordsEqual)
-        "
-      >
-        <div>
-          <label
-            for="register-username"
-            class="block text-sm font-medium leading-6 text-gray-900"
-            >Username</label
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+          <!---bg-green-->
+          <img
+            class="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2
+            class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
           >
-          <div class="mt-2">
-            <input
-              id="register-username"
-              name="username"
-              type="text"
-              v-model="username"
-              autocomplete="username"
-              required
-              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
+            Register your account
+          </h2>
         </div>
-
-        <div>
-          <div class="flex items-center justify-between">
-            <label
-              for="register-Password"
-              class="block text-sm font-medium leading-6 text-gray-900"
-              >Password</label
-            >
-            <div class="text-sm">
-              <a
-                href="#"
-                class="font-semibold text-indigo-600 hover:text-indigo-500"
-                >Forgot password?</a
+        <!---bg-black---->
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <!---bg-red--->
+          <form
+            class="space-y-6"
+            action="#"
+            method="POST"
+            @submit.prevent="
+              store.register(
+                username,
+                password,
+                confirmPassword,
+                arePasswordsEqual
+              )
+            "
+          >
+            <div>
+              <label
+                for="register-username"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >Username</label
               >
+              <div class="mt-2">
+                <input
+                  id="register-username"
+                  name="username"
+                  type="text"
+                  v-model="username"
+                  autocomplete="username"
+                  required
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
             </div>
-          </div>
-          <div class="mt-2">
-            <input
-              id="register-Password"
-              name="password"
-              type="password"
-              v-model="password"
-              autocomplete="current-password"
-              required
-              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-          <div class="mt-7">
-            <label
-              for="register-confirmpassword"
-              class="block text-sm font-medium leading-6 text-gray-900"
-              >Confirm Password</label
+
+            <div>
+              <div class="flex items-center justify-between">
+                <label
+                  for="register-Password"
+                  class="block text-sm font-medium leading-6 text-gray-900"
+                  >Password</label
+                >
+                <div class="text-sm">
+                  <a
+                    href="#"
+                    class="font-semibold text-indigo-600 hover:text-indigo-500"
+                    >Forgot password?</a
+                  >
+                </div>
+              </div>
+              <div class="mt-2">
+                <input
+                  id="register-Password"
+                  name="password"
+                  type="password"
+                  v-model="password"
+                  autocomplete="current-password"
+                  required
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+              <div class="mt-7">
+                <label
+                  for="register-confirmpassword"
+                  class="block text-sm font-medium leading-6 text-gray-900"
+                  >Confirm Password</label
+                >
+
+                <input
+                  id="register-confirmPassword"
+                  name="confirmpassword"
+                  type="password"
+                  v-model="confirmPassword"
+                  autocomplete="current-password"
+                  required
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Register
+              </button>
+            </div>
+          </form>
+        </div>
+
+        <p class="mt-10 text-center text-sm text-gray-500">
+          Already have an account?
+          <RouterLink to="/login">
+            <a
+              href="#"
+              class="font-semibold text-indigo-600 hover:text-indigo-500"
+              >Sign in here</a
             >
-
-            <input
-              id="register-confirmPassword"
-              name="confirmpassword"
-              type="password"
-              v-model="confirmPassword"
-              autocomplete="current-password"
-              required
-              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
-
-        <div>
-          <button
-            type="submit"
-            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Register
-          </button>
-        </div>
-      </form>
-
-      <p class="mt-10 text-center text-sm text-gray-500">
-        Already have an account?
-        <RouterLink to="/login">
-          <a
-            href="#"
-            class="font-semibold text-indigo-600 hover:text-indigo-500"
-            >Sign in here</a
-          >
-        </RouterLink>
-      </p>
+          </RouterLink>
+        </p>
+      </div>
     </div>
-  </div>
+  </body>
 </template>
+
+<style scoped>
+body {
+  background-image: url('https://images.pexels.com/photos/1036808/pexels-photo-1036808.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+  background-size: cover; /* Bild wird proportioniert skaliert, um den gesamten sichtbaren Bereich des Bildschirms zu bedecken */
+  background-position: center; /* Bild wird zentriert positioniert */
+}
+</style>
+>
