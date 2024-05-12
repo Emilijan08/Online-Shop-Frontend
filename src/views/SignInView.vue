1eb9383e96f115b2402b23401588b7f1ebb81a5e
Emilijan Russ <e.russ@bluewin.ch>
2024-05-12 17:42:44 +0200

    Update image in SignInView

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -186,7 +186,7 @@ const store = useAuthStore()
     <div class="relative hidden w-0 flex-1 lg:block">
       <img
         class="absolute inset-0 h-full w-full object-cover"
-        src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+        src="https://images.unsplash.com/photo-1494173853739-c21f58b16055?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3277q=80"
         alt=""
       />
     </div>
