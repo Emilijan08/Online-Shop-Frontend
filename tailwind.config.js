aca38eb2b966456d0ce7ee6f5e70571849232269
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-06-01 19:13:07 +0200

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
+import forms from '@tailwindcss/forms';
+import aspectRatio from '@tailwindcss/aspect-ratio';
+import containerQueries from '@tailwindcss/container-queries';
+
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [
+    typography,
+    forms,
+    aspectRatio,
+    containerQueries,
+  ]
 }
