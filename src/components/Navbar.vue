0324eb2ffbe0b91dfc9303f11706a24d21e614c8
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-06-01 18:32:54 +0200

    Update Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,47 +1,19 @@
-<script setup lang="ts">
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems
-} from '@headlessui/vue'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
-import { computed, ref } from 'vue'
-import { useAuthStore } from '../stores/AuthStore'
-import { useProductStore } from '../stores/ProductsStore'
-import { onMounted } from 'vue'
-
-const auth = useAuthStore()
-const store = useProductStore()
-
-const cartItem = computed(() => {
-  return store.productsOnCart.length > 0 ? true : false
-})
-
-let mobileMenu = ref(false)
-
-
-let Homeclicked = ref(false)
-let Aboutusclicked = ref(false)
-let FAQclicked = ref(false)
-let Whishlistclicked = ref(false)
-let Cartclicked = ref(false)
-
-
-let FAQpageRendered   = ref(false)
-
-onMounted(() => {
-  FAQpageRendered.value = true
-})
-</script>
 <template>
   <Disclosure as="nav" class="bg-white shadow" v-slot="{ open }">
-    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-      <div class="flex h-16 justify-between">
-        <div class="flex">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="relative flex h-16 justify-between">
+        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <!-- Mobile menu button -->
+          <DisclosureButton
+            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+          >
+            <span class="absolute -inset-0.5" />
+            <span class="sr-only">Open main menu</span>
+            <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+            <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
+        </div>
+        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
           <div class="flex flex-shrink-0 items-center">
             <img
               class="h-8 w-auto"
@@ -51,43 +23,40 @@ onMounted(() => {
           </div>
           <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
             <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
-            <RouterLink @click="Homeclicked = true ; Aboutusclicked = false ; FAQclicked = false ; Whishlistclicked = false ; Cartclicked = false" to="/" class="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-900"> 
-              Home <div v-if="Homeclicked" class="border-b-2 border-indigo-500"></div></RouterLink
-             
+            <RouterLink
+              href="#"
+              class="inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900"
+              >Dashboard</RouterLink
             >
-            <RouterLink @click="Homeclicked = false ; Aboutusclicked = true ; FAQclicked = false ; Whishlistclicked = false ; Cartclicked = false"
-              to="/aboutus"
+            <RouterLink
+              href="#"
               class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-              >About Us <div v-if="Aboutusclicked" class="border-b-2 border-indigo-500"></div></RouterLink
+              >Team</RouterLink
             >
-             <RouterLink @click="Homeclicked = false ; Aboutusclicked = false ; FAQclicked = true ; Whishlistclicked = false ; Cartclicked = false"
-            to="/faq"
-             class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700" >
-            <div v-if="FAQclicked" class="border-b-2 border-indigo-500" >FAQ</div>FAQ</RouterLink>
-            <RouterLink @click="Homeclicked = false ; Aboutusclicked = false ; FAQclicked = false ; Whishlistclicked = true ; Cartclicked = false"
-              to="/whishlist"
+            <RouterLink
+              href="#"
               class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-              > <div v-if="Whishlistclicked" class="border-b-2 border-indigo-500"></div>
-              
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-               <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-              </svg>
-
-              </RouterLink
+              >Projects</RouterLink
             >
-            <RouterLink @click="Homeclicked = false ; Aboutusclicked = false ; FAQclicked = false ; Whishlistclicked = false ; Cartclicked = true"
-              to="/cart"
+            <RouterLink
+              href="#"
               class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-              > <div v-if="Cartclicked" class="border-b-2 border-indigo-500"></div>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-              </svg>
-
-              </RouterLink
+              >Calendar</RouterLink
             >
           </div>
         </div>
-        <div class="hidden sm:ml-6 sm:flex sm:items-center">
+        <div
+          class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+        >
+          <button
+            type="button"
+            class="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            <span class="absolute -inset-1.5" />
+            <span class="sr-only">View notifications</span>
+            <BellIcon class="h-6 w-6" aria-hidden="true" />
+          </button>
+
           <!-- Profile dropdown -->
           <Menu as="div" class="relative ml-3">
             <div>
@@ -99,7 +68,7 @@ onMounted(() => {
                 <img
                   class="h-8 w-8 rounded-full"
                   src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt="User profile image"
+                  alt=""
                 />
               </MenuButton>
             </div>
@@ -115,22 +84,22 @@ onMounted(() => {
                 class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
               >
                 <MenuItem v-slot="{ active }">
-                  <a
+                  <RouterLink
                     href="#"
                     :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']"
-                    >Your Profile</a
-                  >
-                </MenuItem>
-                <MenuItem v-slot="{ active }">
-                  <a
-                    href="#"
-                    :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']"
-                    >Settings</a
+                    >Your Profile</RouterLink
                   >
                 </MenuItem>
                 <MenuItem v-slot="{ active }">
                   <RouterLink
-                    to="/auth/login"
+                    href="#"
+                    :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']"
+                    >Settings</RouterLink
+                  >
+                </MenuItem>
+                <MenuItem v-slot="{ active }">
+                  <RouterLink
+                    href="#"
                     :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']"
                     >Sign out</RouterLink
                   >
@@ -139,22 +108,11 @@ onMounted(() => {
             </transition>
           </Menu>
         </div>
-        <div class="-mr-2 flex items-center sm:hidden">
-          <!-- Mobile menu button -->
-          <DisclosureButton
-            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-          >
-            <span class="absolute -inset-0.5" />
-            <span class="sr-only">Open main menu</span>
-            <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
-            <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
-          </DisclosureButton>
-        </div>
       </div>
     </div>
 
     <DisclosurePanel class="sm:hidden">
-      <div class="space-y-1 pb-3 pt-2">
+      <div class="space-y-1 pb-4 pt-2">
         <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" -->
         <DisclosureButton
           as="a"
@@ -181,42 +139,20 @@ onMounted(() => {
           >Calendar</DisclosureButton
         >
       </div>
-      <div class="border-t border-gray-200 pb-3 pt-4">
-        <div class="flex items-center px-4">
-          <div class="flex-shrink-0">
-            <img
-              class="h-10 w-10 rounded-full"
-              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt="User profile image"
-            />
-          </div>
-          <div class="ml-3">
-            <div class="text-base font-medium text-gray-800">Tom Cook</div>
-            <div class="text-sm font-medium text-gray-500">tom@example.com</div>
-          </div>
-      
-        </div>
-        <div class="mt-3 space-y-1">
-          <DisclosureButton
-            as="a"
-            href="#"
-            class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
-            >Your Profile</DisclosureButton
-          >
-          <DisclosureButton
-            as="a"
-            href="#"
-            class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
-            >Settings</DisclosureButton
-          >
-          <DisclosureButton
-            as="a"
-            href="#"
-            class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
-            >Sign out</DisclosureButton
-          >
-        </div>
-      </div>
     </DisclosurePanel>
   </Disclosure>
 </template>
+
+<script setup>
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems
+} from '@headlessui/vue'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+import { RouterLink } from 'vue-router'
+</script>
