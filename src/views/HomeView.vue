7d06d1805ce83cea8176a46ce3a0cf20e1d3c56d
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-06-01 19:46:49 +0200

    Add more components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,13 @@
-<script setup lang="ts">
-import Carousel from '@/components/Carousel.vue'
-import ProductCategories from '@/components/ProductCategories.vue'
-import ProductsPageTailwind from '@/components/ProductsPageTailwind.vue'
-import Testimonials from '@/components/Testimonials.vue'
-</script>
-
 <template>
   <div>
     <ProductCategories class="flex flex-direction:col align-items:start" />
-    <Testimonials />
-    <Carousel />
+    <!-- <Testimonials /> -->
+    <!-- <Carousel /> -->
     <ProductsPageTailwind />
   </div>
 </template>
 
-<style scoped></style>
-@/components/Testimonials.vue
+<script setup lang="ts">
+import ProductCategories from '@/components/ProductCategories.vue'
+import ProductsPageTailwind from '@/components/ProductsPageTailwind.vue'
+</script>
