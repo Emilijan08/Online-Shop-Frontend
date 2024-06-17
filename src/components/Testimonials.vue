35382dce3c7d6d6d10e16871b8ca644acbfbb987
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-06-17 15:45:23 +0200

    Update AboutView

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -1,163 +1,28 @@
-<script setup lang="ts">
-import Splide from '@splidejs/splide'
-import '@splidejs/splide/css/skyblue'
-import { onMounted } from 'vue'
-
-onMounted(() => {
-  new Splide('.splide', {
-    type: 'loop',
-    perPage: 1,
-    gap: '1rem',
-    arrows: true,
-    autoplay: true,
-    interval: 2500,
-    pagination: false,
-    breakpoints: {
-      640: {
-        perPage: 1
-      },
-      768: {
-        perPage: 2
-      },
-      1024: {
-        perPage: 3
-      }
-    }
-  }).mount()
-})
-</script>
-
 <template>
-  <div class="background">
-    <h3 class="flex justify-center items-start z-10">Testimonials</h3>
-    <div class="container flex justify-center">
-      <div class="splide w-full">
-        <div class="splide__track">
-          <div class="splide__list">
-            <div class="splide__slide">
-              <div class="TestimonialsBox">
-                <div class="circleimage-container">
-                  <img
-                    src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                    alt="John Doe"
-                  />
-                </div>
-                <div class="testimonial-content">
-                  <h4>John Doe</h4>
-                  <p>
-                    “I have been using this service for a few months now and I am very happy with
-                    the results. I have seen a significant increase in my website traffic and my
-                    sales have also increased. I would highly recommend this service to anyone who
-                    is looking to increase their online presence.”
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="splide__slide">
-              <div class="TestimonialsBox">
-                <div class="circleimage-container">
-                  <img
-                    src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                    alt="Jane Doe"
-                  />
-                </div>
-                <div class="testimonial-content">
-                  <h4>Jane Doe</h4>
-                  <p>
-                    “This service has transformed my online business. The team is highly
-                    professional and the results speak for themselves. My website traffic has
-                    doubled and my sales have skyrocketed.”
-                  </p>
-                </div>
-              </div>
-            </div>
-            <!-- Weitere Slides hier -->
-          </div>
-        </div>
+  <section class="bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <figure class="mx-auto max-w-2xl">
+      <p class="sr-only">5 out of 5 stars</p>
+      <div class="flex gap-x-1 text-indigo-600">
+        <StarIcon class="h-5 w-5 flex-none" aria-hidden="true" />
+        <StarIcon class="h-5 w-5 flex-none" aria-hidden="true" />
+        <StarIcon class="h-5 w-5 flex-none" aria-hidden="true" />
+        <StarIcon class="h-5 w-5 flex-none" aria-hidden="true" />
+        <StarIcon class="h-5 w-5 flex-none" aria-hidden="true" />
       </div>
-    </div>
-  </div>
+      <blockquote class="mt-10 text-xl font-semibold leading-8 tracking-tight text-gray-900 sm:text-2xl sm:leading-9">
+        <p>“Qui dolor enim consectetur do et non ex amet culpa sint in ea non dolore. Enim minim magna anim id minim eu cillum sunt dolore aliquip. Amet elit laborum culpa irure incididunt adipisicing culpa amet officia exercitation. Eu non aute velit id velit Lorem elit anim pariatur.”</p>
+      </blockquote>
+      <figcaption class="mt-10 flex items-center gap-x-6">
+        <img class="h-12 w-12 rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=1024&h=1024&q=80" alt="" />
+        <div class="text-sm leading-6">
+          <div class="font-semibold text-gray-900">Judith Black</div>
+          <div class="mt-0.5 text-gray-600">CEO of Workcation</div>
+        </div>
+      </figcaption>
+    </figure>
+  </section>
 </template>
 
-<style scoped>
-.h3 {
-  font-size: 100px;
-  color: #ffffff;
-  margin-top: 20px;
-}
-.background {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-image: url('https://images.pexels.com/photos/1420709/pexels-photo-1420709.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  width: 100vw;
-  height: 100vh;
-  min-height: 100vh;
-}
-
-.container {
-  display: flex;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-}
-
-.splide__slide,
-.splide__list,
-.splide__track,
-.splide {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-}
-
-.splide {
-  width: 70%; /* Passe die Breite nach Bedarf an */
-}
-
-.TestimonialsBox {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: #ffffff;
-  padding: 20px;
-  margin: 20px;
-  border-radius: 30px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  text-align: center;
-  font-size: 1.2em;
-  color: #333;
-  height: 100%; /* Höhe der Box */
-  max-height: 400px; /* Maximale Höhe der Box */
-}
-
-.circleimage-container {
-  width: 100px;
-  height: 100px;
-  overflow: hidden;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  clip-path: circle(50%);
-  margin-bottom: 20px;
-}
-
-.testimonial-content h4 {
-  margin-bottom: 10px;
-}
-
-.arrow-image {
-  width: 50px; /* Breite des Pfeilbildes */
-  height: auto; /* Beibehaltung des Seitenverhältnisses */
-}
-
-.flip-vertical {
-  transform: scaleX(-1); /* Bild entlang der Y-Achse spiegeln */
-}
-</style>
+<script setup lang="ts">
+import { StarIcon } from '@heroicons/vue/20/solid';
+</script>
