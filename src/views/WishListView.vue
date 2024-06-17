92f3c4206a59400cd4f39def1ceabe97835b80f2
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-06-17 16:00:15 +0200

    Update WishListView

--- src/views/WishListView.vue
+++ src/views/WishListView.vue
@@ -1,69 +1,113 @@
 <template>
-  <div>
-    <div>
-      <h1 class="h1">WishList</h1>
+  <div class="md:flex md:items-center md:justify-between m-6 md:m-10">
+    <div class="min-w-0 flex-1">
+      <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">Whishlist</h2>
     </div>
-    <div class="WhishlistThing">
-      <div>
-        <img
-          id="ProductImage"
-          src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-01.jpg"
-          alt="ProductImage"
-        />
-      </div>
-      <div id="ProductName">
-        <p>the Whislisted Products name</p>
-      </div>
-      <div id="Heart">
-        <img
-          src="https://www.psdgraphics.com/wp-content/uploads/2022/01/perfect-heart-shape.png"
-          alt="Heart"
-        />
-      </div>
+    <div class="mt-4 flex md:ml-4 md:mt-0">
+      <button type="button"
+        class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Edit</button>
+      <button type="button"
+        class="ml-3 inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Publish</button>
     </div>
   </div>
+
+
+  <ul role="list" class="divide-y divide-gray-100 overflow-hidden bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl m-6 md:m-10">
+    <li v-for="person in people" :key="person.email" class="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50 sm:px-6">
+      <div class="flex min-w-0 gap-x-4">
+        <img class="h-12 w-12 flex-none rounded-full bg-gray-50" :src="person.imageUrl" alt="" />
+        <div class="min-w-0 flex-auto">
+          <p class="text-sm font-semibold leading-6 text-gray-900">
+            <a :href="person.href">
+              <span class="absolute inset-x-0 -top-px bottom-0" />
+              {{ person.name }}
+            </a>
+          </p>
+          <p class="mt-1 flex text-xs leading-5 text-gray-500">
+            <a :href="`mailto:${person.email}`" class="relative truncate hover:underline">{{ person.email }}</a>
+          </p>
+        </div>
+      </div>
+      <div class="flex shrink-0 items-center gap-x-4">
+        <div class="hidden sm:flex sm:flex-col sm:items-end">
+          <p class="text-sm leading-6 text-gray-900">{{ person.role }}</p>
+          <p v-if="person.lastSeen" class="mt-1 text-xs leading-5 text-gray-500">
+            Last seen <time :datetime="person.lastSeenDateTime">{{ person.lastSeen }}</time>
+          </p>
+          <div v-else class="mt-1 flex items-center gap-x-1.5">
+            <div class="flex-none rounded-full bg-emerald-500/20 p-1">
+              <div class="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+            </div>
+            <p class="text-xs leading-5 text-gray-500">Online</p>
+          </div>
+        </div>
+        <ChevronRightIcon class="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+      </div>
+    </li>
+  </ul>
 </template>
 
-<style scoped>
-.h1 {
-  font-size: 50px;
-  color: rgb(94, 101, 102);
-  font-weight: bold;
-  font-family: Arial, sans-serif;
-  text-align: center;
-  margin: 0;
-  padding: 0;
-}
+<script setup lang="ts">
+import { ChevronRightIcon } from '@heroicons/vue/20/solid';
 
-#ProductImage {
-  width: 100px;
-  height: 100px;
-  border: 1px solid rgb(255, 255, 255);
-}
-
-#Heart {
-  width: 60px;
-  height: 46px;
-  border: 1px solid rgb(255, 255, 255);
-  display: flex;
-  margin-left: auto;
-  margin-right: 10px;
-}
-
-.WhishlistThing {
-  border: 1px solid rgb(0, 0, 0);
-  border-left: 1px solid rgb(255, 255, 255);
-  border-right: 1px solid rgb(255, 255, 255);
-  display: flex;
-  align-items: center;
-}
-
-#ProductName {
-  border-left: 1px solid rgb(255, 255, 255);
-  padding-left: 10px;
-  font-size: 30px;
-  font-weight: bold;
-  font-family: Arial, sans-serif;
-  color: rgb(94, 101, 102);
-}
-</style>
+const people = [
+  {
+    name: 'Leslie Alexander',
+    email: 'leslie.alexander@example.com',
+    role: 'Co-Founder / CEO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    href: '#',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Michael Foster',
+    email: 'michael.foster@example.com',
+    role: 'Co-Founder / CTO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    href: '#',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Dries Vincent',
+    email: 'dries.vincent@example.com',
+    role: 'Business Relations',
+    imageUrl:
+      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    href: '#',
+    lastSeen: null,
+  },
+  {
+    name: 'Lindsay Walton',
+    email: 'lindsay.walton@example.com',
+    role: 'Front-end Developer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    href: '#',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Courtney Henry',
+    email: 'courtney.henry@example.com',
+    role: 'Designer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    href: '#',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Tom Cook',
+    email: 'tom.cook@example.com',
+    role: 'Director of Product',
+    imageUrl:
+      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    href: '#',
+    lastSeen: null,
+  },
+]
+</script>
