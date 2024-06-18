bb18d7dcc3705c0ca507284ba592db82707831a3
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-06-18 18:19:55 +0200

    Update ProductsList

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -6,21 +6,24 @@
       <div
         class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8"
       >
-        <a v-for="product in products" :key="product.id" :href="product.href" class="group">
+        <router-link
+          v-for="(product, index) in store.products"
+          :key="index"
+          :to="`/products/${product._id}`"
+          class="group"
+        >
           <div
             class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7"
           >
-            <router-link :to="'/products/${productId}'">
-              <img
-                :src="store.productImage"
-                :alt="store.productName"
-                class="h-full w-full object-cover object-center group-hover:opacity-75"
-              />
-            </router-link>
+            <img
+              :src="product.productImage"
+              :alt="product.productName"
+              class="h-full w-full object-cover object-center group-hover:opacity-75"
+            />
           </div>
-          <h3 class="mt-4 text-sm text-gray-700">{{ store.productName }}</h3>
-          <p class="mt-1 text-lg font-medium text-gray-900">{{ store.price }}</p>
-        </a>
+          <h3 class="mt-4 text-sm text-gray-700">{{ product.productName }}</h3>
+          <p class="mt-1 text-lg font-medium text-gray-900">{{ product.price }}</p>
+        </router-link>
       </div>
     </div>
   </div>
@@ -30,39 +33,4 @@
 import { useProductStore } from '@/stores/ProductsStore'
 
 const store = useProductStore()
-const products = [
-  {
-    id: 1,
-    name: 'Earthen Bottle',
-    href: '#',
-    price: '$48',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
-    imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.'
-  },
-  {
-    id: 2,
-    name: 'Nomad Tumbler',
-    href: '#',
-    price: '$35',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
-    imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.'
-  },
-  {
-    id: 3,
-    name: 'Focus Paper Refill',
-    href: '#',
-    price: '$89',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
-    imageAlt: 'Person using a pen to cross a task off a productivity paper card.'
-  },
-  {
-    id: 4,
-    name: 'Machined Mechanical Pencil',
-    href: '#',
-    price: '$35',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
-    imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.'
-  }
-  // More products...
-]
 </script>
