fb6795cda5b44badd8be5248d35da207e6bb883e
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-06-23 01:31:07 +0200

    Update ProductDetailsView

--- src/views/ProductDetailsView.vue
+++ src/views/ProductDetailsView.vue
@@ -11,31 +11,20 @@
     >
       <!-- Product details -->
       <div class="lg:max-w-lg lg:self-end">
-        <div class="mt-4">
-          <!--HeartIcon-->
-          <!-- <div
-            class="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 flex justify-end items-start"
-            id="Heart"
-            @click="toggleWishlistAction(productId)"
-          > -->
-          <div
-            class="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 flex justify-end items-start"
-            id="Heart"
-          >
-            <span class="absolute -inset-1.5" />
-            <span class="sr-only">View wishlist</span>
-            <HeartIcon :fill="ClickedOnAddToWishlist ? 'red' : 'none'" />
-          </div>
+        <div class="mt-4 flex justify-between">
           <h1 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            {{ product[0].productName }}
+            {{ product?.productName }}
           </h1>
+          <button @click="toggleWishlist" :class="wishlistButtonClass">
+            <HeartIcon class="h-6 w-6" aria-hidden="true" />
+          </button>
         </div>
 
         <section aria-labelledby="information-heading" class="mt-4">
           <h2 id="information-heading" class="sr-only">Product information</h2>
 
           <div class="flex items-center">
-            <p class="text-lg text-gray-900 sm:text-xl">{{ product[0].price }}</p>
+            <p class="text-lg text-gray-900 sm:text-xl">{{ product?.price }}</p>
 
             <div class="ml-4 border-l border-gray-300 pl-4">
               <h2 class="sr-only">Reviews</h2>
@@ -60,7 +49,7 @@
           </div>
 
           <div class="mt-4 space-y-6">
-            <p class="text-base text-gray-500">{{ product[0].productDescription }}</p>
+            <p class="text-base text-gray-500">{{ product?.productDescription }}</p>
           </div>
 
           <div class="mt-6 flex items-center">
@@ -74,8 +63,8 @@
       <div class="mt-10 lg:col-start-2 lg:row-span-2 lg:mt-0 lg:self-center">
         <div class="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg">
           <img
-            :src="product[0].productImage"
-            :alt="product[0].productName"
+            :src="product?.productImage"
+            :alt="product?.productName"
             class="h-full w-full object-cover object-center"
           />
         </div>
@@ -87,53 +76,6 @@
           <h2 id="options-heading" class="sr-only">Product options</h2>
 
           <form>
-            <div class="sm:flex sm:justify-between">
-              <!-- Size selector -->
-              <!-- <fieldset>
-                <legend class="block text-sm font-medium text-gray-700">Size</legend>
-                <RadioGroup
-                  v-model="selectedSize"
-                  class="mt-1 grid grid-cols-1 gap-4 sm:grid-cols-2"
-                >
-                  <RadioGroupOption
-                    as="template"
-                    v-for="size in product.sizes"
-                    :key="size.name"
-                    :value="size"
-                    :aria-label="size.name"
-                    :aria-description="size.description"
-                    v-slot="{ active, checked }"
-                  >
-                    <div
-                      :class="[
-                        active ? 'ring-2 ring-indigo-500' : '',
-                        'relative block cursor-pointer rounded-lg border border-gray-300 p-4 focus:outline-none'
-                      ]"
-                    >
-                      <p class="text-base font-medium text-gray-900">{{ size.name }}</p>
-                      <p class="mt-1 text-sm text-gray-500">{{ size.description }}</p>
-                      <div
-                        :class="[
-                          active ? 'border' : 'border-2',
-                          checked ? 'border-indigo-500' : 'border-transparent',
-                          'pointer-events-none absolute -inset-px rounded-lg'
-                        ]"
-                        aria-hidden="true"
-                      />
-                    </div>
-                  </RadioGroupOption>
-                </RadioGroup>
-              </fieldset> -->
-            </div>
-            <div class="mt-4">
-              <a href="#" class="group inline-flex text-sm text-gray-500 hover:text-gray-700">
-                <span>What size should I buy?</span>
-                <QuestionMarkCircleIcon
-                  class="ml-2 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-                  aria-hidden="true"
-                />
-              </a>
-            </div>
             <div class="mt-10">
               <button
                 type="submit"
@@ -161,69 +103,70 @@
 <script setup lang="ts">
 import { useAuthStore } from '@/stores/AuthStore'
 import { useProductStore } from '@/stores/ProductsStore'
-import {
-  CheckIcon,
-  ChevronLeftIcon,
-  QuestionMarkCircleIcon,
-  StarIcon
-} from '@heroicons/vue/20/solid'
-import { HeartIcon, ShieldCheckIcon } from '@heroicons/vue/24/outline'
+import { useWishlistStore } from '@/stores/WishlistStore'
+import type { ProductType } from '@/types/Product'
+import { CheckIcon, ChevronLeftIcon, HeartIcon, StarIcon } from '@heroicons/vue/20/solid'
+import { ShieldCheckIcon } from '@heroicons/vue/24/outline'
 import axios from 'axios'
-import { ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 
-const store = useProductStore()
-const authStore = useAuthStore()
 const route = useRoute()
+const productId = route.params.productId as string
 
+const productStore = useProductStore()
+const authStore = useAuthStore()
+const wishlistStore = useWishlistStore()
+
+const product = ref<ProductType | null>(null)
 const addProduct = ref(false)
 const comment = ref('')
+const reviews = { average: 4, totalCount: 1624 }
 
-const productId = route.params.productId
-const product = store.products.filter((element) => element._id === productId)
+onMounted(async () => {
+  if (!product.value) {
+    await productStore.getProducts()
+    product.value = productStore.products.find((element) => element._id === productId) || null
+  }
+  await wishlistStore.getWishlist()
+})
 
-console.log(productId)
-store.getComments(productId as string)
+const toggleWishlist = async () => {
+  if (product.value) {
+    if (isInWishlist.value) {
+      await wishlistStore.removeFromWishlist(product.value._id)
+    } else {
+      await wishlistStore.addToWishlist(product.value._id)
+    }
+  }
+}
 
-const ClickedOnAddToWishlist = ref(false)
+const isInWishlist = computed(() =>
+  wishlistStore.wishlist.some((item) => item.productId === product?.value?._id)
+)
 
-// function addProductToWishlist() {
-//   store.products.ProductsOnWishlist.push(product)
-// }
-
-// function removeProductFromWishlist(productId: string | string[]) {
-//   store.products.ProductsOnWishlist = store.products.ProductsOnWishlist.filter(
-//     (product:string | string[]) => product.id !== productId
-//   )
-// }
-
-// function toggleWishlistAction(productId: string | string[]) {
-//   if (ClickedOnAddToWishlist.value) {
-//     removeProductFromWishlist(productId)
-//   } else {
-//     addProductToWishlist(productId: string | string[])
-//   }
-//   ClickedOnAddToWishlist.value = !ClickedOnAddToWishlist.value
-// }
+const wishlistButtonClass = computed(() => [
+  'p-2 rounded-full focus:outline-none',
+  isInWishlist.value ? 'text-red-500' : 'text-gray-400 hover:text-gray-500'
+])
 
 async function addComment() {
   try {
-    const URL = `https://marketserver.onrender.com/products/${productId}/comments`
-    const response = await axios.post(URL, {
-      comment: comment.value,
-      username: authStore.user.username
-    })
-
-    console.log(response)
+    if (product.value) {
+      const URL = `https://marketserver.onrender.com/products/${product.value._id}/comments`
+      const response = await axios.post(URL, {
+        comment: comment.value,
+        username: authStore.user.username
+      })
+      console.log(response)
+    }
   } catch (err) {
     console.log(err)
   } finally {
     comment.value = ''
-    await store.getComments(productId as string)
+    if (product.value) {
+      await productStore.getComments(product.value._id)
+    }
   }
 }
-
-const reviews = { average: 4, totalCount: 1624 }
-
-// const selectedSize = ref(product.sizes[0])
 </script>
