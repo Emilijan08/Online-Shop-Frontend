9e54ff21fda59935326a98bda9310b218243c656
Emilijan Russ <e.russ@bluewin.ch>
2024-06-22 12:34:34 +0200

    Update LoginErrorModelDialog Content/Style and create <RouterLink>

--- src/components/LoginErrorModelDialog.vue
+++ src/components/LoginErrorModelDialog.vue
@@ -41,30 +41,32 @@
               </div>
               <div class="sm:flex sm:items-start">
                 <div
-                  class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"
+                  class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-indigo-600 sm:mx-0 sm:h-10 sm:w-10"
                 >
-                  <ExclamationTriangleIcon class="h-6 w-6 text-red-600" aria-hidden="true" />
+                  <ExclamationTriangleIcon class="h-6 w-6 text-white" aria-hidden="true" />
                 </div>
                 <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                   <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900"
-                    >Deactivate account</DialogTitle
+                    >Incorrect username or password.</DialogTitle
                   >
                   <div class="mt-2">
                     <p class="text-sm text-gray-500">
-                      Are you sure you want to deactivate your account? All of your data will be
-                      permanently removed from our servers forever. This action cannot be undone.
+                      We could not find an account with that username and password. Check your
+                      username and password and try again, or if you don't have an account, register
                     </p>
                   </div>
                 </div>
               </div>
               <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-                <button
-                  type="button"
-                  class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-                  @click="closeLoginErrorModelDialog"
-                >
-                  Deactivate
-                </button>
+                <RouterLink to="/auth/register">
+                  <button
+                    type="button"
+                    class="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                    @click="closeLoginErrorModelDialog"
+                  >
+                    Register an account
+                  </button>
+                </RouterLink>
                 <button
                   type="button"
                   class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
