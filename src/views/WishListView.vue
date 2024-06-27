64dd4fdb631a6c780ad26c6c1640f6ee4bc52cea
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-06-27 09:52:38 +0200

    Update WishListView

--- src/views/WishListView.vue
+++ src/views/WishListView.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="md:flex md:items-center md:justify-between m-6 md:m-10">
     <div class="min-w-0 flex-1">
-      <h2
-        class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight"
-      >
+      <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
         Wishlist
       </h2>
     </div>
@@ -23,15 +21,19 @@
     class="divide-y divide-gray-100 overflow-hidden bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl m-6 md:m-10"
   >
     <li
-      v-for="(wishlistItem, index) in wishlistStore.wishlist"
+      v-for="(wishlistItem, index) in uniqueWishlist"
       :key="index"
       class="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50 sm:px-6"
     >
-      <div class="flex min-w-0 gap-x-4">
-        <img class="h-12 w-12 flex-none bg-gray-50" :src="wishlistItem.product.image" alt="" />
+      <div class="flex min-w-0 gap-x-4" v-if="wishlistItem.product">
+        <img
+          class="h-12 w-12 flex-none bg-gray-50"
+          :src="wishlistItem.product.image"
+          alt=""
+        />
         <div class="min-w-0 flex-auto">
           <p class="text-sm font-semibold leading-6 text-gray-900">
-            <router-link :to="`/products/${wishlistItem.productId}`">
+            <router-link :to="`/products/${wishlistItem.product._id}`">
               <span class="absolute inset-x-0 -top-px bottom-0" />
               {{ wishlistItem.product.name }}
             </router-link>
@@ -44,8 +46,8 @@
       <div class="flex shrink-0 items-center gap-x-4">
         <button
           type="button"
-          class="inline-flex items-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
-          @click="removeFromWishlist(wishlistItem.productId)"
+          class="inline-flex items-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 z-50"
+          @click="removeFromWishlist(wishlistItem.product._id)"
         >
           Remove
         </button>
@@ -55,8 +57,8 @@
 </template>
 
 <script setup lang="ts">
-import { useWishlistStore } from '@/stores/WishlistStore'
-import { onMounted } from 'vue'
+import { useWishlistStore } from '@/stores/WishlistStore';
+import { computed, onMounted } from 'vue';
 
 const wishlistStore = useWishlistStore()
 
@@ -71,4 +73,14 @@ const removeFromWishlist = (productId: string) => {
 const clearWishlist = () => {
   wishlistStore.clearWishlist()
 }
+
+const uniqueWishlist = computed(() => {
+  const uniqueItems = wishlistStore.wishlist.reduce((acc, item) => {
+    if (!acc.find(accItem => accItem.product._id === item.product._id)) {
+      acc.push(item)
+    }
+    return acc;
+  }, [] as typeof wishlistStore.wishlist)
+  return uniqueItems;
+})
 </script>
